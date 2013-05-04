1d70c27a77798e61fdde3e85a01cee1969cf2018
/dev/null <devnull@base48.cz>
2013-05-04 16:32:34 +0200

    higher X/Y-MAX-PO

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -254,9 +254,9 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 #define min_software_endstops true //If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  //If true, axis won't move to coordinates greater than the defined lengths below.
 // Travel limits after homing
-#define X_MAX_POS 180
+#define X_MAX_POS 195
 #define X_MIN_POS 0
-#define Y_MAX_POS 190
+#define Y_MAX_POS 195
 #define Y_MIN_POS 0
 #define Z_MAX_POS 90
 #define Z_MIN_POS 0
